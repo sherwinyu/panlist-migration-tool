d2c0eb632a7b73875432d7ec884f3934d678e3c6
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-12-10 16:05:42 -0500

    Initial commit + add specs (mdown)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plmt::Application.config.secret_token = 'bba189db701f6e3d6972c39a2246c7a4482704f3b4d0e85607c6116ae1e1eb9a06cf79b9c552bfe404a3ebdd10f4caf40a707240b17c95df18b181fec2c154b9'
