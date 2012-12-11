5319d50a25833d77ce6f2f5373867691bc3d8f0d
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-12-10 19:33:26 -0500

    add members method (for parsing the 'subscribers' flat string) and num_members to panlist.rb; update dashboard view

--- app/models/panlist.rb
+++ app/models/panlist.rb
@@ -1,4 +1,13 @@
 class Panlist < ActiveRecord::Base
   attr_accessible :migrated, :name, :public, :subscribers
   has_and_belongs_to_many :owners
+
+  def num_members 
+    subscribers.size
+  end
+
+  def members
+    subscribers.split
+  end
+
 end
