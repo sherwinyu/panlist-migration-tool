bc403c441efe644747b1b00f58f5972e6ecaabb0
Adam Bray <adam.bray@yale.edu>
2012-12-13 15:50:28 -0500

    modified panlists a bit to match legacy database schema

--- app/models/panlist.rb
+++ app/models/panlist.rb
@@ -1,6 +1,9 @@
 class Panlist < ActiveRecord::Base
+
+  #Setup connection to legacy postgres database
   establish_connection "ahi_fake"
   self.table_name = 'lists'
+  alias_attribute :name, :list_name
 
   attr_accessible :migrated, :name, :public, :subscribers
   has_and_belongs_to_many :owners
