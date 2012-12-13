d9ef0734c51eebb5579e581623750643a90cc586
Adam Bray <adam.bray@yale.edu>
2012-12-13 15:45:54 -0500

    panlist model connects to table properly

--- app/models/panlist.rb
+++ app/models/panlist.rb
@@ -1,5 +1,6 @@
 class Panlist < ActiveRecord::Base
   establish_connection "ahi_fake"
+  self.table_name = 'lists'
 
   attr_accessible :migrated, :name, :public, :subscribers
   has_and_belongs_to_many :owners
